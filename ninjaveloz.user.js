c00b98ccafbb5d930bb9af6c65db40babb85bf1b
StreetDogArg <StreetDogArg@users.noreply.github.com>
2016-02-25 23:25:04 -0300

    Update ninjaveloz.user.js

--- ninjaveloz.user.js
+++ ninjaveloz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        NinjaVeloz
 // @namespace   raina ft. StreetDogArg
-// @description Script que hace saltar la ventana de Activación de Keys en Steam cuando detecta una Key en el portapapeles, haciendo posible un mejor Ninjeo o más cómodo para 
+// @description Script que hace saltar la ventana de Activación de Keys en Steam cuando detecta una Key en el portapapeles, haciendo posible un mejor Ninjeo o más cómodo para cargar las keys que compraste en sitios, ver README para los sitios soportados. Este Script REQUIERE STEAM CORRIENDO.
 // @include     /^https?:\/\/www\.humblebundle\.com\/downloads\?/
 // @include     /^https?:\/\/www\.humblebundle\.com\/home\/(keys|library)/
 // @include     /^https?:\/\/www\.indieroyale\.com\/bundle\/key\//
@@ -15,8 +15,7 @@
 // @include     /^https?:\/\/www\.greenmangaming\.com\/user\/account/
 // @include     /^https:\/\/secure\.nuuvem\.com\/account\/library/
 // @include     /^http:\/\/www\.dlh\.net\/en\/steam-keys\.html/
-// @include     /^http:\/\/www\.taringa\.net\/
-// @include     /^https:\/\/www\.taringa\.net\/
+// @include     /^https?:\/\/www\.taringa\.net\/
 // @version     1
 // @grant       none
 // ==/UserScript==
