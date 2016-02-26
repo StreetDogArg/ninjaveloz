6e4d5c58d39589249292b04ac6861d2624446d83
StreetDogArg <StreetDogArg@users.noreply.github.com>
2016-02-26 02:46:35 -0300

    Update ninjaveloz.user.js

--- ninjaveloz.user.js
+++ ninjaveloz.user.js
@@ -4,7 +4,8 @@
 // @colaborator StreetDogARG
 // @namespace   raina ft. StreetDogARG
 // @homepage    https://www.taringa.net/comunidades/juegos-originales-gratis-steam
-// @defaulticon https://zebra.ninja/favicon.ico
+// @defaulticon data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAJiYlAK/N+gAZBfcAPT09AJmZmQANBwAAbjQfANvq/wAFDQAA1tbWAAAAAAAAAAAAAAAAAAAAAAAAAAAAZHdmaqVmAABHdmZmqlYAAHdmZmZqpQAAZmZmZmaqUABmZmZmYAqlAAZmZmYAAKpQZmZmZmAAClBmEWEWYAAKpWYWZhZmAAClZmYWZmYAAKUoiGiIJgAACipqimopAAAKlmZmmZYAAAARmZkRMAAAADMzMzMAAAAAAGZmYDMAAAAADwAAAA8AAAAPAAAABwAAAGMAAIDxAAAAeQAAAHgAAAA8AAAAPAAAAD4AAAA+AAAAPwAAAH8AAAD/AADBPwAA
+// @icon64      data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAJiYlAK/N+gAZBfcAPT09AJmZmQANBwAAbjQfANvq/wAFDQAA1tbWAAAAAAAAAAAAAAAAAAAAAAAAAAAAZHdmaqVmAABHdmZmqlYAAHdmZmZqpQAAZmZmZmaqUABmZmZmYAqlAAZmZmYAAKpQZmZmZmAAClBmEWEWYAAKpWYWZhZmAAClZmYWZmYAAKUoiGiIJgAACipqimopAAAKlmZmmZYAAAARmZkRMAAAADMzMzMAAAAAAGZmYDMAAAAADwAAAA8AAAAPAAAABwAAAGMAAIDxAAAAeQAAAHgAAAA8AAAAPAAAAD4AAAA+AAAAPwAAAH8AAAD/AADBPwAA
 // @description Script que hace saltar la ventana de Activación de Keys en Steam cuando detecta una Key en el portapapeles, haciendo posible un mejor Ninjeo o más cómodo para cargar las keys que compraste en sitios, ver README para los sitios soportados. Este Script REQUIERE STEAM CORRIENDO.
 // ------------ Sitios de compra
 // @include     /^https?:\/\/www\.humblebundle\.com\/downloads\?/
@@ -36,15 +37,15 @@
 // ==/UserScript==
 
 (function() {
-	"use strict";
-	var activateProduct = function(e) {
-		var productCode = window.getSelection().toString().trim();
-		if (!productCode) {
-			productCode = e.target.value;
-		}
-		if (/^[\d\w]{2,5}(\-[\d\w]{4,5}){2,4}$/.test(productCode)) {
-			window.location.href = "steam://open/activateproduct";
-		}
-	};
-	window.addEventListener("copy", activateProduct, false);
+    "use strict";
+    var activateProduct = function(e) {
+        var productCode = window.getSelection().toString().trim();
+        if (!productCode) {
+            productCode = e.target.value;
+        }
+        if (/^[\d\w]{2,5}(\-[\d\w]{4,5}){2,4}$/.test(productCode)) {
+            window.location.href = "steam://open/activateproduct";
+        }
+    };
+    window.addEventListener("copy", activateProduct, false);
 }());
