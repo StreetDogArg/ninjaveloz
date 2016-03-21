78384f55bb9a9c179605dda349f4417093521294
StreetDogArg <StreetDogArg@users.noreply.github.com>
2016-03-21 16:52:38 -0300

    Update ninjaveloz.user.js

--- ninjaveloz.user.js
+++ ninjaveloz.user.js
@@ -8,45 +8,10 @@
 // @icon64      data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAJiYlAK/N+gAZBfcAPT09AJmZmQANBwAAbjQfANvq/wAFDQAA1tbWAAAAAAAAAAAAAAAAAAAAAAAAAAAAZHdmaqVmAABHdmZmqlYAAHdmZmZqpQAAZmZmZmaqUABmZmZmYAqlAAZmZmYAAKpQZmZmZmAAClBmEWEWYAAKpWYWZhZmAAClZmYWZmYAAKUoiGiIJgAACipqimopAAAKlmZmmZYAAAARmZkRMAAAADMzMzMAAAAAAGZmYDMAAAAADwAAAA8AAAAPAAAABwAAAGMAAIDxAAAAeQAAAHgAAAA8AAAAPAAAAD4AAAA+AAAAPwAAAH8AAAD/AADBPwAA
 // @description Script que hace saltar la ventana de Activación de Keys en Steam cuando detecta una Key en el portapapeles, haciendo posible un mejor Ninjeo o más cómodo para cargar las keys que compraste en sitios, ver README para los sitios soportados. Este Script REQUIERE STEAM CORRIENDO.
 // ------------ Sitios de compra
-// @include     /^https?:\/\/www\.humblebundle\.com\/downloads\?/
-// @include     /^https?:\/\/www\.humblebundle\.com\/home\/(keys|library)/
-// @include     /^https?:\/\/www\.indieroyale\.com\/bundle\/key\//
-// @include     /^https?:\/\/www\.indiegala\.com/(profile|game)\?/
-// @include     /^https?:\/\/groupees\.com\/purchases/
-// @include     /^https?:\/\/www\.bundlestars\.com\/en\/orders/
-// @include     /^https?:\/\/(www\.)?flyingbundle\.com\/users\/account/
-// @include     /^https?:\/\/(www\.)?dailyindiegame\.com\/account_page\.html/
-// @include     /^https?:\/\/www\.greenmangaming\.com\/user\/account/
-// @include     /^https:\/\/secure\.nuuvem\.com\/account\/library/
-// @include     *://www.tremorgames.com/message/*
-// ------------ Sitios de sorteos, giveaways, etc
-// @include     /^https?:\/\/www\.steamgifts\.com\/giveaways\/won/
-// @include     /^https?:\/\/steamcompanion\.com\/gifts\/won/
-// @include     /^http:\/\/www\.dlh\.net\/en\/steam-keys\.html/
-// @include     *://gameminer.net/messages/*
-// @include     *://www.tremorgames.com/message/*
-// @include     *://www.listia.com/account/message/*
-// @include     *://www.listia.com/auction/*
-// @include     *://www.gleam.io/*
-// @include     *://gleam.io/*
-// ------------ Sitios de ninjeo
-// @include     *://steamcommunity.com/*
-// @include     *://www.taringa.net/*
-// @include     *://whosgamingnow.net/discussion/*
-// @include     *://www.steamgifts.com/discussion/*
-// @include     *://www.facebook.com/*
-// @include     *://www.twitter.com/*
-// @include     *.chatango.com/*
-// @include     *://www.hrkgame.com/*
-// @include     *://www.bundlestars.com/*
-// @include     *://www.dlh.net/*
-// ------------ Otros (pesonales, mails)
-// @include     https://*.mail.live.com/*
-// @include     https://outlook.live.com/*
-// ------------ Incluiré más sitios con el tiempo, a medida que me lo recomienden.
+// @include     *
 // @updateURL   https://github.com/StreetDogArg/ninjaveloz/raw/master/ninjaveloz.user.js
 // @downloadURL https://github.com/StreetDogArg/ninjaveloz/raw/master/ninjaveloz.user.js
-// @version     1.6
+// @version     0
 // @grant       none
 // ==/UserScript==
 
